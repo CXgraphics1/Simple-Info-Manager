c407d4a774c3ab9e9aedfc284a6dbe2e2cee2dd5
CXgraphics1 <csinica12@gmail.com>
2022-04-27 19:58:33 -0400

    Fixed Dumb Error

--- src/core/update.js
+++ src/core/update.js
@@ -24,7 +24,7 @@ module.exports.selectPassword = async () => {
     });
 
     this.updatePassword(`_${password.password}`);
-    closeSync();
+    closeSync(0);
     return password.password;
 };
 
